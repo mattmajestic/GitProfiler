dbfd50e1890cef27b3a7decb0790c9f57d533b5f
Majestic Coding <33904170+mattmajestic@users.noreply.github.com>
2024-05-04 20:07:29 -0400

    basic django app

---
+++ gitprofiler/gitprofiler/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from . import views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Gitproilfer API",
+        default_version='v1',
+        description="API documentation for Gitproilfer project",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+     path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+     path('app/', views.gitprofiler),
+     path('download/', views.download_report),
+]
